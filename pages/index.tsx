28a2a92db3ffb0bbfe82c2ad5e5dbd2df768d6eb
Costeira <joaocurveira13@gmail.com>
2022-08-12 12:04:34 +0100

    banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,18 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Banner from '../components/Banner'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Medium</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
+      <Banner />
     </div>
   )
 }
